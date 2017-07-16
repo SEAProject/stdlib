f3fc44529b6e896790ca8241dcfa6f1d726a9c10
Administrator@VDI.ASYNCHRONOUS.LAB <Administrator@VDI.ASYNCHRONOUS.LAB>
2017-07-16 02:35:22 -0700

    Prototype methods for string

--- string.pm
+++ string.pm
@@ -2,6 +2,7 @@ package stdlib::string;
 use strict;
 use warnings;
 require Exporter;
+use stdlib::array;
 use vars qw(@EXPORT @ISA);
 
 @ISA = qw(Exporter);
@@ -11,7 +12,7 @@ sub new {
     my ($class,$str) = @_;
     my $blessed = bless({ freezed => 0 },ref($class) || $class);
     eval {
-        $blessed->set($str);
+        $blessed->updateValue($str);
     };
     die $@ if $@;
     return $blessed;
@@ -22,7 +23,7 @@ sub freeze {
     $self->{freezed} = 1;
 }
 
-sub set {
+sub updateValue {
     my ($self,$newValue) = @_;
     return if $self->{freezed};
     eval {
@@ -31,7 +32,7 @@ sub set {
             $self->{_value} = "$newValue";
         }
         elsif($ref eq "stdlib::string") {
-            $self->{_value} = $newValue->get();
+            $self->{_value} = $newValue->valueOf;
         }
         else {
             warn "Invalid string type!\n";
@@ -40,9 +41,126 @@ sub set {
     warn $@ if $@;
 }
 
-sub value {
+sub valueOf {
     my ($self) = @_;
     return $self->{_value};
+}
+
+sub isEqual {
+    my ($self,$value) = @_;
+    return 0 if !defined $value;
+    return 1 if $self->{_value} eq $value;
+}
+
+sub slice {
+    my ($self,$start,$end) = @_;
+}
+
+sub substr {
+    my ($self,$startPosition,$length) = @_;
+    if(!defined $startPosition) {
+        $startPosition = 0;
+    }
+    if(!defined $length) {
+        $length = 1;
+    }
+    return substr( $self->{_value} , $startPosition, $length );
+}
+
+sub charAt {
+    my ($self,$index) = @_;
+    return undef if !defined $index; 
+    return $self->substr( $index );
+}
+
+sub charCodeAt {
+    my ($self,$index) = @_;
+    return undef if !defined $index; 
+    return ord( $self->charAt( $index ) );
+}
+
+sub endsWith {
+    my ($self,$strSeeked) = @_;
+}
+
+sub concat {
+    my $self = shift;
+    foreach(@_) {
+        my $ref = ref($_) || ref(\$_);
+        if($ref eq "SCALAR") {
+            $self->{_value} .= "$_";
+        }
+    }
+}
+
+sub contains {
+    my ($self,$str) = @_;
+}
+
+sub split {
+    my ($self,$splitCaracter) = @_; 
+    return if !defined $splitCaracter;
+    return stdlib::array->new(split($splitCaracter,$self->{_value}));
+}
+
+sub repeat {
+    my ($self,$repeatCount) = @_;
+    if(!defined $repeatCount) {
+        $repeatCount = 1;
+    }
+    my $repeatedValue = $self->{_value};
+    while($repeatCount--) {
+        $self->{_value} .= $repeatedValue;
+    }
+    return $self;
+}
+
+sub replace {
+    my ($self,$originChar,$focusChar) = @_;
+    return $self if !defined $originChar;
+    if(!defined $focusChar) {
+        $focusChar = '';
+    }
+    $self->{_value} =~ s/$originChar/$focusChar/g;
+    return $self;
+}
+
+sub toLowerCase {
+    my ($self) = @_;
+    $self->{_value} = lc $self->{_value};
+    return $self;
+}
+
+sub toUpperCase {
+    my ($self) = @_;
+    $self->{_value} = uc $self->{_value};
+    return $self;
+}
+
+sub trim {
+    my ($self) = @_;
+    $self->{_value} =~ s/^\s+|\s+$//g;
+    return $self;
+}
+
+sub trimRight {
+    my ($self) = @_;
+    $self->{_value} =~ s/\s+$//;
+    return $self;
+}
+
+sub trimLeft {
+    my ($self) = @_;
+    $self->{_value} =~ s/^\s+//;
+    return $self;
+}
+
+sub padEnd {
+
+}
+
+sub padStart {
+
 }
 
 sub isString {
